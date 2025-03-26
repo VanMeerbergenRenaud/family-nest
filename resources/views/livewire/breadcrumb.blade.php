67f8650eb737a0419f8a1856fa53ad2d71d2cbde
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-03-26 16:14:46 +0100

    Breadcrumb spacing fix

--- resources/views/livewire/breadcrumb.blade.php
+++ resources/views/livewire/breadcrumb.blade.php
@@ -1,11 +1,11 @@
 <div class="relative p-4">
     <nav class="lg:pl-5 flex items-center" aria-label="Fil d'Ariane">
         <h1 class="sr-only" role="heading" aria-level="1">Menu de navigation</h1>
-        <ol class="inline-flex items-center space-x-2">
+        <ol class="inline-flex items-center gap-2">
             @foreach($segments as $index => $segment)
-                <li class="inline-flex items-center space-x-2">
+                <li class="inline-flex items-center gap-2">
                     @if($index > 0)
-                        <svg class="w-5 h-5 -ml-2 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                        <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                         </svg>
                     @endif
