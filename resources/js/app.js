905963eabd91738dc6ac8c2e5c4e1fd01a8b0caa
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-06-18 15:55:42 +0200

    Remove the Chart.js CDN fro production and use npm instead

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,7 @@ import './bootstrap';
 import '../../vendor/masmerise/livewire-toaster/resources/js';
 
 import ui from '@alpinejs/ui';
-
 Alpine.plugin(ui);
+
+import Chart from 'chart.js/auto';
+window.Chart = Chart;
